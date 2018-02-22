e357c8d00ef2f2508e76ced1c2c0cdb1d0ad1b64
lastnamearya <arya.jigyasu6815@gmail.com>
2018-02-23 02:33:47 +0530

    code structured for toggle list button

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
+// First get the Hide List Button which will toggle our List
 const toggleList = document.getElementById('toggleList');
+
+// Second, get the List which we want to toggle Completely
 const listDiv = document.querySelector('.list');
 
+// Now, Select the Input Element
 const input = document.querySelector('input.description');
 
+// Paragraph Element i.e going to mutate
 const p = document.querySelector('p.description');
 
+// Button for Changing the List Description
 const button = document.querySelector('button.description');
 
+// On Clicking the first Button i.e ToggleList Button, Event fires up
 toggleList.addEventListener('click', () => {
+  
   if (listDiv.style.display == "none") {
       toggleList.textContent = "Hide List";
       listDiv.style.display = 'block';
