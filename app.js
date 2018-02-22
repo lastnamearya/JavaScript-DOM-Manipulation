bac16cfa774b03e1b3747cf256d6af23f510bf5d
lastnamearya <arya.jigyasu6815@gmail.com>
2018-02-23 00:01:01 +0530

    getElementsByClassName

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const myList = document.getElementsByTagName('li');
 
-myList[2].style.color = "red";
-
 for(let i = 0; i < myList.length; i++){
   myList[i].style.color = 'purple';
 }
 
+ const errorNotPurple = document.getElementsByClassName('error-not-purple');
+
+ for(let i = 0; i < errorNotPurple.length; i++){
+   errorNotPurple[i].style.color = "red";
+ }
