fc67ab3df1a4da9bd150a2c158176f1191721b5f
lastnamearya <arya.jigyasu6815@gmail.com>
2018-02-23 14:58:50 +0530

    createElement

--- app.js
+++ app.js
@@ -5,27 +5,42 @@ const toggleList = document.getElementById('toggleList');
 const listDiv = document.querySelector('.list');
 
 // Now, Select the Input Element
-const input = document.querySelector('input.description');
+const descriptionInput = document.querySelector('input.description');
 
 // Paragraph Element i.e going to mutate
-const p = document.querySelector('p.description');
+const descriptionP = document.querySelector('p.description');
 
 // Button for Changing the List Description
-const button = document.querySelector('button.description');
+const descriptionButton = document.querySelector('button.description');
+
+// Input for adding a new list item by user
+const addItemInput = document.querySelector('input.addItemInput');
+
+// Button for adding a new list item
+const addItemButton = document.querySelector('button.addItemButton');
+
+// ******************************************* //
 
 // On Clicking the first Button i.e ToggleList Button, Event fires up
 toggleList.addEventListener('click', () => {
   
   if (listDiv.style.display == "none") {
+    debugger;
       toggleList.textContent = "Hide List";
       listDiv.style.display = 'block';
   } else {
+    debugger;
       toggleList.textContent = "Show List";
       listDiv.style.display = 'none';
   }
-})
+});
 
-button.addEventListener('click', () => {
-  p.innerHTML = input.value + ":";
-})
+descriptionButton.addEventListener('click', () => {
+  descriptionP.innerHTML = descriptionInput.value + ":";
+});
 
+// Event Listener for our add new List Item Button
+addItemButton.addEventListener('click', () => {
+  let li = document.createElement('li');
+  li.textContent = addItemInput.value;
+});
